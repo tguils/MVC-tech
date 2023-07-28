6fcf6254dd4a213bab8c1a6cc4742e9df27caa00
tguils <tess.guilmette@gmail.com>
2023-07-28 19:21:19 -0400

    more bug fixes with heroku logs

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
