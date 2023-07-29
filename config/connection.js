5813a33fb065d0b7e45e695d4768f2f5fbfee437
tguils <tess.guilmette@gmail.com>
2023-07-29 16:22:29 -0400

    reconfiguring connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,32 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql', 
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+} else {
+  // For local development or other environments
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: 'mysql', 
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+    }
+  );
+}
+
+module.exports = sequelize;
+
 
 module.exports = sequelize;
