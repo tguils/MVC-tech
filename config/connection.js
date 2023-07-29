70ebe5fbb1c962a7a6e243fdd1ade030373e7784
tguils <tess.guilmette@gmail.com>
2023-07-29 16:35:19 -0400

    config fix

--- config/connection.js
+++ config/connection.js
@@ -2,32 +2,22 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql', 
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
-} else {
-  // For local development or other environments
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
+const sequelize = process.env.HEROKU_POSTGRESQL_MAUVE_URL
+  ? new Sequelize(process.env.HEROKU_POSTGRESQL_MAUVE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      dialect: 'mysql', 
+      dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-    }
-  );
-}
-
-module.exports = sequelize;
-
+    });
 
 module.exports = sequelize;
